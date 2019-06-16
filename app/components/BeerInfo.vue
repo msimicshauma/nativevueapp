e33d5359f06e1674aaf45324b38af53a32450a2a
Milos Simic <msimic@Miloss-MacBook-Pro-2.local>
2019-06-16 14:34:52 +0200

    add infinite scroll

--- app/components/BeerInfo.vue
+++ app/components/BeerInfo.vue
@@ -1,15 +1,26 @@
 <template>
     <Page>
-      <ActionBar title="Beer Info"/>
+        <ActionBar :title="beer.name"/>
+        <ScrollView orientation="vertical">
+            <StackLayout orientation="vertical">
+                <img :src="beer.image_url" class="beer-img"/>
+                <Label 
+                    :text="beer.description" 
+                    class="beer-desc"
+                    textWrap="true"
+                ></Label>
+            </StackLayout>
+        </ScrollView>
     </Page>
 </template>
 
 <script>
   export default {
-    data() {
-      return {
-        
-      }
+    props: {
+        beer: {
+            type: Object,
+            required: true
+        }
     }
   }
 </script>
@@ -18,5 +29,18 @@
     ActionBar {
         background-color: #674172;
         color: #fff;
+    }
+
+    .beer-desc {
+        padding: 40px 40px 40px 40px;
+        border-top-color: #888;
+        border-top-width: 2px;
+        color: #888;
+    }
+
+    .beer-img {
+        margin-top: 60px;
+        margin-bottom: 60px;
+        width: 200px;
     }
 </style>
--- app/components/BeerList.vue
+++ app/components/BeerList.vue
@@ -1,40 +1,90 @@
 <template>
-    <ListView for="beer in beers">
-        <v-template>
-            <StackLayout @tap="$navigateTo(BeerInfo)" class="beerItem">
-                <Label :text="beer.name"></Label>
-                <Label :text="beer.tagline"></Label>
-            </StackLayout>
-        </v-template>
-   </ListView>
+    <PullToRefresh @refresh="refreshList">
+        <ListView for="beer in beers" @loadMoreItems="getBeers">
+            <v-template>
+                <FlexboxLayout 
+                    class="beer-item"
+                    flexDirection="row"
+                    justifyContent="space-between"
+                    @tap="loadBeer(beer)"
+                > 
+                    <StackLayout orientation="vertical">
+                        <Label :text="beer.name" textWrap="true"></Label>
+                        <Label :text="beer.tagline" textWrap="true"></Label>
+                    </StackLayout>
+                    <img :src="beer.image_url" class="beer-img"/>
+                </FlexboxLayout>
+            </v-template>
+        </ListView>
+    </PullToRefresh>
 </template>
 
 <script>
   import BeerInfo from './BeerInfo'
+  import Vue from 'nativescript-vue';
+ 
+  Vue.registerElement(
+    'PullToRefresh',
+    () => require('nativescript-pulltorefresh').PullToRefresh
+  );
+
   export default {
     data() {
       return {
-        beers: null
+        beers: [],
+        page: 1,
+        loadMore: true
       }
     },
     created() {
-      fetch('https://api.punkapi.com/v2/beers')
-      .then(res => {
-        return res.json()
-      })
-      .then(beerData => {
-        this.beers = beerData
-      })
-    }
+      this.getBeers()
+    },
+    methods: {
+        refreshList(args) {
+            var pullRefresh = args.object;
+            this.beers = []
+            this.page = 1
+
+            this.getBeers()
+            .then(res => {
+                setTimeout(() => {
+                    pullRefresh.refreshing = false;
+                }, 1000);
+            })
+        },
+        getBeers() {
+            return new Promise((resolve, reject) => {
+                fetch(`https://api.punkapi.com/v2/beers?page=${this.page}&per_page=15`)
+                .then(res => {
+                    return res.json()
+                })
+                .then(beersList => {
+                    this.beers = this.beers.concat(beersList)
+                    this.page++
+                    resolve('success')
+                })
+                .catch(error => {
+                    reject(error)
+                })
+            })
+        },
+        loadBeer(beer) {
+            this.$navigateTo(BeerInfo, { props: { beer } })
+        }
+    },
   }
 </script>
 
 <style scoped>
-    .beerItem {
+    .beer-item {
       text-align: left;
       padding: 20px 20px 20px 20px;
       border-bottom-color: #888;
       border-bottom-width: 2px;
       color: #888;
+    }
+
+    .beer-img {
+        width: 30px;
     }
 </style>
